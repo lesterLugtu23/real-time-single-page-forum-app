41c11b76bb024df41f8caf8476b6af4029c89882
Lester Alvin Manuel Lugtu <lesteralvinmanuellugtu@gmail.com>
2020-11-27 01:33:09 +0800

    Creating factory and seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+        \App\Models\Category::factory(5)->create();
+        \App\Models\Question::factory(10)->create();
+        \App\Models\Reply::factory(50)->create()
+            ->each(function($reply) {
+                return $reply->like()->save(\App\Models\Like::factory()->make());
+            });
     }
 }
