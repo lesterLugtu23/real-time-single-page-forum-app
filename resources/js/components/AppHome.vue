648da78a4776640e37356f9daccaecb4c83e11fb
Lester Alvin Manuel Lugtu <lesteralvinmanuellugtu@gmail.com>
2020-11-30 05:44:22 +0800

    Vuetify, vue router and initial components

---
+++ resources/js/components/AppHome.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <toolbar></toolbar>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+import AppFooter from './AppFooter'
+import Toolbar from './Toolbar'
+export default {
+    components:{Toolbar, AppFooter}
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Real Time Single Page Forum</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
+    <div id="app">
+        <v-app>
+            <app-home></app-home>
+        </v-app>
+    </div>
+    <script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
